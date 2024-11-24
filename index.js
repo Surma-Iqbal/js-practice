67044b44d6e1673ce7cc25e2a2f818e8d4c6df78
Surma-Iqbal <surmaiqbal@gmail.com>
2024-11-25 01:23:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,163 @@
+//console.log("Hi! I am practicing node.js")
+
+
+ //1 for loop
+
+/* for(i=0;i<100;i=i+1){
+let btn="<button>submit</button><br>"
+    console.log (btn)
+}
+for(i=0;i<100;i=i+1){
+    console.log (i+"<br/>")
+   }
+  
+   
+   2 while loop
+
+   var x=0
+while(x<100){
+    console.log(x+"from while loop<br/>")
+    x=x+1;   
+}
+
+
+3. do while loop
+var y=0
+do{
+    console.log(y+"from do while loop<br/>")
+    y=y+1;   
+}
+while(y<100)
+
+4.regular function
+
+function addTwo(){
+    let num1=5;
+    let num2=10;
+    let sum=num1+num2;
+    console.log(sum)
+    }
+    addTwo()
+   
+    4. parameter
+   
+    function addTwo(a,b){
+    let num1=a;
+    let num2=b;
+    let sum=num1+num2;
+    console.log(sum)
+    }
+    addTwo(15,25)
+   
+    5. function return
+   
+    function naya (){
+       return "hello";
+    }
+    let res=naya()+5
+    console.log(res)
+   
+    6.Array function
+
+    naya =()=>{
+        return "50";
+     }
+     let res= naya ()+5
+     console.log(res)
+
+7.Immediately invoked function (IIF)
+
+( ( )=> {
+    console.log("I am IIF")
+ })()
+
+
+8. Anonymous function
+
+  let anygivenname =()=>{
+    console.log("I am anonymous")
+  }
+   anygivenname();
+
+9.a. if
+
+ var age=30;
+ if (age>18){
+    console.log("you are an adult")
+}
+    var age=30;
+   var city="Dhaka";
+   if (age>18 && city=="Dhaka"){
+    console.log("you are an adult")
+}
+9.b. if else
+ 
+ var age=20;
+ var city="Dhaka";
+ if (age>18 && city=="Dhaka"){
+    console.log("you are an adult")
+ }
+else{
+    console.log("you are an child")   
+
+}
+9.c. if else if
+
+var marks=84;
+if(marks <=100 && marks >=80){
+    console.log("A+");
+}
+else if(marks <80 && marks >=70){
+    console.log("A");
+}
+else if(marks <70 && marks >=60){
+    console.log("A-");
+}
+else if(marks <60 && marks >=50){
+    console.log("B");
+}
+else if(marks <50 && marks >=40){
+    console.log("C");
+}
+else if(marks <40 && marks >=33){
+    console.log("D");
+}
+else{
+    console.log("F");
+}
+
+10. Switch case
+
+var marks=14;
+switch(true){
+
+case(marks <=100 && marks >=80):
+console.log("A+");
+break;
+
+case(marks <80 && marks >=70):
+console.log("A");
+break;
+
+case(marks <70 && marks >=60):
+console.log("A-");
+break;
+
+case(marks <60 && marks >=50):
+console.log("B");
+break;
+
+case(marks <50 && marks >=40):
+console.log("C");
+break;
+
+case(marks <40 && marks >=33):
+console.log("D");
+break;
+
+default:
+    console.log("F");
+}*/
+
+
+
