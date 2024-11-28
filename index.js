67c88d17f7d923f8b4f7d2e5ba4561091f392a6e
Surma-Iqbal <surmaiqbal@gmail.com>
2024-11-29 00:43:11 +0600

    Array and string method

--- index.js
+++ index.js
@@ -158,7 +158,10 @@ break;
 default:
     console.log("F");
 }
-11. for loop over Array
+
+11.a. for loop over Array, b. Array forEach method, c. Array concat, d. Array from, e. Array Includes,
+   f. Array IndexOf, g. Array pop,push,reverse, h.Array sort,sort reverse and slice, splice
+a. for loop over Array
 
 var category=["milk","tea","coffee","juice"];
 
@@ -167,17 +170,140 @@ for(i=0;i<category.length;i=i+1){
 console.log(category[i]+"<br>")
 }
 
-12. Array forEach method
+b. Array forEach method
 
 var numArray=[32,43,55,67,87,93,99];
 numArray.forEach(function (item){
 console.log(item+"<br>")
 })
 
-13. Array concate
+c. Array concat
 
 var arr1=["aaraf","zayan","faizan","farzad"];
 var arr2=["naya","guddi"];
 var arr3=arr1.concat(arr2);
-console.log(arr3)*/
+console.log(arr3)
 
+d. Array from
+
+var title="Learn with Rabbi";
+var titleArray=Array.from(title);
+console.log(titleArray[11])
+
+e. Array Includes
+
+var numArray=[20,30,48,57,68,78,98];
+var result=numArray.includes(100);
+console.log(result)
+
+f. Array IndexOf
+
+var numArray=[20,30,48,57,68,78,98];
+var result=numArray.indexOf(100);
+console.log(result)
+
+
+g. Array pop,push,reverse
+
+i.pop
+
+var numArray=[32,43,54,65,76,87,98];
+numArray.pop();
+console.log(numArray)
+
+ii.push
+
+var numArray=[32,43,54,65,76,87,98];
+numArray.push(100);
+numArray.push(189);
+console.log(numArray)
+
+iii.reverse
+
+var numArray=[32,43,54,65,76,87,98];
+var result=numArray.reverse();
+console.log(result)
+
+h.Array sort,sort reverse and slice, splice
+
+i.sort
+var numArray=[22,99,44,55,66,33,11];
+numArray.sort();
+console.log(numArray)
+
+ii.sort.reverse
+
+var numArray=[22,99,44,55,66,33,11];
+numArray.sort().reverse();
+console.log(numArray)
+
+iii.slice
+
+var numArray=[32,43,54,65,76,87,98];
+var result=numArray.slice(1,3);
+console.log(result)
+
+iv.Array splice (remove)
+
+var numArray=[32,43,54,65,76,87,98];
+numArray.splice(1,3);
+console.log(numArray)
+
+v.Array splice (add)
+
+var numArray=[32,43,54,65,76,87,98];
+numArray.splice(1,3,100);
+console.log(numArray)
+
+12. String Method{a.CharAt(),b.concat(),c.indexOf(),d.lastIndexOf,e.replace(),f.substring(),g.slice()
+      h.LowerCase(),i.UpperCase(),j.trim()}
+
+a.charAt()
+var country = "Bangladesh";
+console.log(country.charAt(0));
+
+b.concat()
+var country1 = "Bangladesh";
+var country2 = "India";
+console.log(country1.concat(country2))
+
+c.indexOf()
+var country = "Bangladesh";
+console.log(country.indexOf("g"))
+
+d.lastIndex()
+var country = "Bangladesh";
+console.log(country.lastIndexOf("a"))
+
+e.replace()
+var country = "Bangladesh";
+console.log(
+    country.replace("desh","-DESHI"))
+
+f.substring()
+var country = "Bangladesh";
+console.log(
+    country.substring(6,10))
+
+
+g.slice()
+var country = "Bangladesh";
+console.log(
+    country.slice(0,6))
+
+h.LowerCase()
+var country = "Bangladesh";
+console.log(
+    country.toLowerCase())
+
+i.UpperCase()
+
+var country = "Bangladesh";
+console.log(
+    country.toUpperCase())
+
+j.trim()
+var country1="   Bangladesh   ";
+var country2=country1.trim();
+console.log(
+    country1.length)*/
